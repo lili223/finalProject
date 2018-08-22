06885af54f400f0c095665c9021c2a6271285e6e
lili223 <36684738+lili223@users.noreply.github.com>
2018-08-21 23:43:27 -0400

    Update getinfo.php

--- html/getinfo.php
+++ html/getinfo.php
@@ -1,22 +1,20 @@
+
+
 <?php
-echo 11;
+//receiving email and password from the form
 $email = $_REQUEST['email'];
-$password = $_REQUEST['pwd'];
-
-require_once 'login.php';
-$conn = new mysqli($hn, $un, $pw, $db); 
- if ($conn->connect_error) 
-     die($conn->connect_error); 
-
-$sql    = 'SELECT email,password FROM `users` WHERE email = "'.$email.'" and password ="'.$password.'"';
-$result = mysql_query($sql, $link);
-echo $result;
+$password = $_REQUEST['password'];
+//connecting to database to check whether the login combo (username and password //) exists or not.
+require('login.php'); 
+$conn = new mysqli($hn,$un,$pw,$db);
+  if ($conn->connect_error) die($conn->connect_error);
+$query = 'SELECT email,password FROM `users` WHERE email = "'.$email.'" and password ="'.$password.'"';
+$result = $conn->query($query);
 $hint .= "username and password do not match our records or do not exists";
  $result = $conn->query($query);
  if (!$result) die ("Database access failed: " . $conn->error);
 $rows = $result->num_rows;
 $hint .= "username and password do not match our records or do not exists";
-if ($rows ==1)
-$hint = "ok";
+if ($rows ==1)$hint = "";
 echo $hint;
 ?>
